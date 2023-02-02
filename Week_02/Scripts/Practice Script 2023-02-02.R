7e7a98b7a70dd02ddc7114e66f771a84b66824c1
Leah Reichert <jane@example.com>
2023-02-02 15:18:22 -0800

    test

--- Week_02/Scripts/Practice Script 2023-02-02.R
+++ Week_02/Scripts/Practice Script 2023-02-02.R
@@ -2,3 +2,11 @@
 ### Created by: Leah Reichert
 ### Created on: 2023-02-02
 ###############################################
+### Load libraries ########## 
+library(tidyverse)
+library(here)
+WeightData<-read_csv(here("Week_02","Data","weightdata.csv"))
+head(WeightData) #Look at the top 6 lines of the dataframe
+tail(WeightData) #Looks at the bottom 6 lines of the dataframe 
+View(WeightData) #opens a new window to look at the entire dataframe
+glimpse(WeightData)
