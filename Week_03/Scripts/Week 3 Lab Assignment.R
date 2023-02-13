5b5ff716029e0d210bf7ee9389a9a0e00d35a695
LeahReichert <124097566+LeahReichert@users.noreply.github.com>
2023-02-12 17:35:46 -0800

    Week_03

---
+++ Week_03/Scripts/Week 3 Lab Assignment.R
@@ -0,0 +1,26 @@
+### Today we are going to plot penguin data ####
+### Created by: Leah Reichert #############
+### Updated on: 2023-02-12 ####################
+# load the following packages before starting 
+library(tidyverse)
+library(palmerpenguins)
+library(here)
+library(beyonce)
+library(ggthemes)
+# body mass and sex for each adelie species 
+glimpse(penguins) #load data 
+summary(penguins)
+
+quantify_data<-ggplot(data=penguins,
+                      mapping=aes(x=sex,y=body_mass_g,
+                                  fill=sex))+
+  geom_boxplot()+
+  labs(x="sex",y="Body Mass (g)",
+       title="Quantify the Body Mass of Penguins")
+facet_grid(~species)+ 
+  scale_fill_manual(values=beyonce_palette(7))+
+  theme_stata()+
+  theme(axis.title=element_text(size=15),
+        panel.background==element_rect("gray"))
+ggsave(here("Week_03","output","sex.png"),
+       width=7, height=5)
