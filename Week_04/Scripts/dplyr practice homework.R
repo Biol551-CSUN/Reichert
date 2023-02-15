02fc798ddf3e930eec08c6f7e4c0273e11937b2c
Leah Reichert <jane@example.com>
2023-02-14 16:10:45 -0800

    please work

--- Week_04/Scripts/dplyr practice homework.R
+++ Week_04/Scripts/dplyr practice homework.R
@@ -5,6 +5,10 @@
 library(palmerpenguins) # load data 
 library(tidyverse) # load package 
 library(here) # load package 
+library(beyonce) # load palette 
+library(ggthemes) #load package
+
+
 glimpse(penguins) # view data 
 
 #### Calculate Mean ####
@@ -24,22 +28,70 @@ Female_Data <- penguins %>% # group penguin data
 
 #### Part 2 ####
 
-Female_Data <- penguins %>% # use penguin dataframe
+Female_Dataplot <- penguins %>% # use penguin dataframe
   filter(sex == "female") %>% #select females
   mutate(log_mass = log(body_mass_g)) %>% # calculate log 
   select(species, island, sex, log_mass) # select categories 
-library(beyonce) # load palette 
-ggplot(data =Female_Data, # make a graph 
-  mapping=aes(x=species, # x axis 
-              y=log_mass, # y axis 
-              fill=sex)) + # boxes are filled based on sex 
-  geom_boxplot()+ # type of graph 
-  labs(x="Females", # title of x axis 
-       y="Log Mass", # title of y axis 
-       title= "Homework Week 4") + # title of graph 
-  facet_wrap(~species) # splits grapg by species 
 
-ggsave(Female_Data,here("Week_04","Output","Penguin.png"), # saved 
+ggplot(data =Female_Dataplot, # make a graph 
+  mapping=aes(x=species, # x axis 
+              y=log_mass,# y axis 
+              color = species, # added color to the species 
+              fill=species)) + # boxes are filled based on sex 
+  geom_boxplot()+ # type of graph### Today we are going to learn about dyplr: Homework ####
+### Created by: Leah Reichert #############
+### Updated on: 2022-02-15 ####################
+#### Load Libraries ######
+library(palmerpenguins) # load data 
+library(tidyverse) # load package 
+library(here) # load package 
+library(beyonce) # load palette 
+library(ggthemes) #load package
+
+
+glimpse(penguins) # view data 
+
+#### Calculate Mean ####
+
+penguins %>% # group penguin data 
+  drop_na(species,island,sex) %>% # remove NA from these groups 
+  group_by(species,island, sex) %>% # group categories together 
+  summarise(body_mass_g = mean(body_mass_g, na.rm = TRUE)) # calculate mean body mass 
+
+#### Calculate Variance ####
+
+Female_Data <- penguins %>% # group penguin data 
+  drop_na(species,island,sex) %>% # remove NA from these groups 
+  group_by(species,island, sex) %>% # group categories together 
+  summarise(body_mass_g = var(body_mass_g, na.rm = TRUE)) # calculate variance of body mass 
+
+
+#### Part 2 ####
+
+Female_Dataplot <- penguins %>% # use penguin dataframe
+  filter(sex == "female") %>% #select females
+  mutate(log_mass = log(body_mass_g)) %>% # calculate log 
+  select(species, island, sex, log_mass) # select categories 
+
+ggplot(data =Female_Dataplot, # make a graph 
+       mapping=aes(x=species, # x axis 
+                   y=log_mass,# y axis 
+                   color = species,
+                   fill=species)) + # boxes are filled based on sex 
+  geom_boxplot()+ # type of graph 
+  labs(x="species", # title of x axis 
+       y="body_mass_g", # title of y axis 
+       title= "Penguin Female Data Only") + # title of graph 
+  facet_wrap(~island)
+
+ggsave(here("Week_04","Output","Penguin_hw.png"), # saved 
+       width=7, height=5) 
+  labs(x="species", # title of x axis 
+       y="body_mass_g", # title of y axis 
+       title= "Penguin Female Data Only") + # title of graph 
+  facet_wrap(~island) # seperated by island
+ 
+ggsave(here("Week_04","Output","Penguin_hw.png"), # saved 
        width=7, height=5)
 
 
